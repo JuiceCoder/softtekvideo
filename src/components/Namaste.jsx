89146f0e4ab3427d6150975bbcb5334a66fc09d6
Jose Armando Quiñonez <juiceqm@gmail.com>
2019-08-27 17:28:35 -0700

    ESLINT Configuration

---
+++ src/components/Namaste.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../assets/styles/App.scss'
+
+export const Namaste = () => {
+  return (
+    <h1>Namaste</h1>
+  )
+}
+
+
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Namaste } from "./components/Namaste";
+
+ReactDOM.render(<Namaste />, document.getElementById('app'));
